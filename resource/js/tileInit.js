2ca6b02a45c019f549b06b67c48213d6c95f11f2
tianxia <hellosummer0604@gmail.com>
2017-02-16 17:15:41 +0800

    -add init table

--- resource/js/tileInit.js
+++ resource/js/tileInit.js
@@ -6,7 +6,7 @@ jQuery.noConflict();
 var Init = Init || {}
 
 jQuery(window).ready(function () {
-    Init._setMapSize();
+    Init._setMapSize(6, 10);
     Init._setTileCategories(3);
     Init._initEmptyMap();
 
@@ -36,9 +36,17 @@ Init._defauleTileCategories = [{
 
 
 //设置地图尺寸
-Init._setMapSize = function () {
-    this.mapWidth = 6;
-    this.mapHeight = 10;
+Init._setMapSize = function (width, height) {
+    if (typeof width == "undefined" || width < 3 || width > 10) {
+        width = 10;
+    }
+
+    if (typeof height == "undefined" || height < 3 || height > 10) {
+        height = 10;
+    }
+
+    this.mapWidth = width;
+    this.mapHeight = height;
 }
 
 //设置有几种花色,最少三种
@@ -50,16 +58,43 @@ Init._setTileCategories = function (count) {
     this._tileCategories = Init._defauleTileCategories.slice(0, count);
 }
 
-//创建为空的地图
+//创建为空的地图（map entity）
 Init._initEmptyMap = function () {
-    this.mainMap = new Array();
+    this.mapEntity = new Array();
 
     for (var i = 0; i < this.mapHeight; i++) {
-        this.mainMap[i] = new Array();
+        this.mapEntity[i] = new Array();
         for (var j = 0; j < this.mapWidth; j++) {
-            this.mainMap[i][j] = 0;
+            this.mapEntity[i][j] = null;
         }
     }
+
+    Init._refreshMapDisplay(this.mapEntity);
+}
+
+//将map entity的数据填充到显示层的table中
+Init._refreshMapDisplay = function (obj) {
+    if (typeof obj == "undefined") {
+        console.error("map entity not found");
+    }
+
+    var htmlStr = "";
+
+    for (var i = 0; i < obj.length; i++) {
+        htmlStr += "<tr>";
+        for (var j = 0; j < obj[i].length; j++) {
+            htmlStr += "<td>";
+            if (obj[i][j] == null) {
+                htmlStr += "233";
+            } else {
+
+            }
+            htmlStr += "</td>";
+        }
+        htmlStr += "</tr>";
+    }
+
+    jQuery("#gameMap").html(htmlStr);
 }
 
 Init.generateRandomTile = function () {
